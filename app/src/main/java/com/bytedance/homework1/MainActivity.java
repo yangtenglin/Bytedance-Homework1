9c0b1b03e8b8283862268541cdce9ccd35da474f
xiaoxiaoyang <2867834391@qq.com>
2025-11-23 09:27:44 +0800

    课后作业1 初始化

---
+++ app/src/main/java/com/bytedance/homework1/MainActivity.java
@@ -0,0 +1,37 @@
+package com.bytedance.homework1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.bytedance.homework1.ui.userinfo.ProfileActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        findViewById(R.id.login_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/bytedance/homework1/ui/userinfo/ProfileActivity.java
@@ -0,0 +1,15 @@
+package com.bytedance.homework1.ui.userinfo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bytedance.homework1.R;
+
+public class ProfileActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
